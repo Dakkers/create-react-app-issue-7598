8ff7271dc70539987108868590840f0ff823b6c5
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2019-10-03 10:54:49 -0400

    Update to reflect own code

---
+++ src/jsx/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Home.scss';
+
+function App() {
+  return (
+    <div>
+      <div className="donkey-kong"></div>
+
+      <h1>Homepage</h1>
+    </div>
+  );
+}
+
+export default App;
